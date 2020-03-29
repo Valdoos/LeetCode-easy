fb70f43dce73126acf96a55caa683c61cd6a9e76
Vlad Fishbain <44998764+Valdoos@users.noreply.github.com>
2020-03-29 13:48:42 +0300

    Create palindrome-number.go

---
+++ palindrome-number.go
@@ -0,0 +1,11 @@
+func isPalindrome(x int) bool {
+    if x < 0 || (x % 10 == 0 && x != 0) {
+        return false;
+    }
+    var num int
+    for x > num {
+        num = num*10 + x%10
+        x=x/10
+    }
+    return x == num || x == num/10
+}
