4cabe5d41845db5ac265cef1289be2b2d1d300c4
Vlad Fishbain <44998764+Valdoos@users.noreply.github.com>
2020-04-05 11:20:10 +0300

    Update pascals-triangle-ii.go

--- pascals-triangle-ii.go
+++ pascals-triangle-ii.go
@@ -1,15 +1,14 @@
+
 func getRow(rowIndex int) []int {
     if rowIndex == 0 {
         return []int{1}
     }
     row := make([]int,rowIndex+1)
-    temp := make([]int,rowIndex+1)
     row[0]=1
     for i:=1;i<=rowIndex;i++{
-         copy(temp,row)
          row[i]=1
-         for j:=1;j<i;j++{
-             row[j]=temp[j-1]+temp[j]
+         for j:=i-1;j>0;j--{
+             row[j]=row[j-1]+row[j]
          }
 
     }
