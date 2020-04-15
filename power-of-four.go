c03c6b373663708608a7c8ae721bd778e9feb87c
Vlad Fishbain <44998764+Valdoos@users.noreply.github.com>
2020-04-15 12:55:58 +0300

    Update power-of-four.go

--- power-of-four.go
+++ power-of-four.go
@@ -3,3 +3,7 @@ func isPowerOfFour(n int) bool {
     lg := math.Log2(float64(n))/2
     return lg-float64(int(lg))==0
 }
+
+func isPowerOfFour(n int) bool {
+    return n>0 && 1073741824 % n == 0 && ( n==1 || n%10 == 4 || n%10 == 6)
+}
