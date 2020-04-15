206fb2f59c371acd5d60d468b2b5af3ca72056fb
Vlad Fishbain <44998764+Valdoos@users.noreply.github.com>
2020-04-15 12:57:16 +0300

    Update power-of-two.go

--- power-of-two.go
+++ power-of-two.go
@@ -1,13 +1,3 @@
 func isPowerOfTwo(n int) bool {
-    if n < 0 {
-        return false
-    }
-    count:=0
-    for n!=0{
-        if n&1==1{
-            count++
-        }
-        n=n>>1
-    }
-    return count==1
+   return n>0 && 1073741824 % n == 0
 }
