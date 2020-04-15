a85f282e548d3675ba5e77c1f63606529c4064d8
Vlad Fishbain <44998764+Valdoos@users.noreply.github.com>
2020-04-15 20:19:00 +0300

    Create .intersection-of-two-arrays.go

---
+++ .intersection-of-two-arrays.go
@@ -0,0 +1,14 @@
+func intersection(nums1 []int, nums2 []int) []int {
+    hash := make(map[int]struct{},len(nums1)+len(nums2))
+    ans := make([]int,0,len(nums1))
+    for _,n := range nums1 {
+        hash[n]=struct{}{}
+    }
+    for _,n := range nums2 {
+        if _,ok := hash[n];ok{
+           ans = append(ans,n)
+            delete(hash,n)
+        }
+    }
+    return ans
+}
