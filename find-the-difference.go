161c65f40f2dc1f5d1bd2d0ee2a528b3444484f7
Vlad Fishbain <44998764+Valdoos@users.noreply.github.com>
2020-04-15 23:02:22 +0300

    Create find-the-difference.go

---
+++ find-the-difference.go
@@ -0,0 +1,10 @@
+func findTheDifference(s string, t string) byte {
+    var ans byte
+    for _,ch := range []byte(s) {
+        ans^=ch
+    }
+     for _,ch := range []byte(t) {
+        ans^=ch
+    }
+    return ans
+}
