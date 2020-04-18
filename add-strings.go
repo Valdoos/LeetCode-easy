2ea63880c36353dd98e25c70d3fee6f2cb805751
Vlad Fishbain <44998764+Valdoos@users.noreply.github.com>
2020-04-18 14:58:22 +0300

    Update add-strings.go

--- add-strings.go
+++ add-strings.go
@@ -1,33 +1,44 @@
 import "fmt"
 func addStrings(num1 string, num2 string) string {
-    const max = 5100
-    str1 := make([]byte,max-len(num1),max)
-    str1 = []byte(string(str1)+num1)
-    str2 := make([]byte,max-len(num2),max)
-    str2 = []byte(string(str2)+num2)
-    ans := make([]byte,max)
-    l := helper(str1,str2,ans,max-1,maxInt(len(num1),len(num2)),0)
-    return string(ans[l+1:])
-}
-func maxInt(a, b int) int {
-    if a > b {
-        return a
-    } 
-    return b
+
+    n1 := &num1
+    n2 := &num2
+    if len(num1) < len(num2) {
+        n1,n2 = n2,n1
+    }
+    l1 := len(*n1)
+    l2 := len(*n2)
+    l := max(l1,l2)+1
+    ans := make([]byte,l)
+    var supple byte
+    l1,l2,l = l1-1,l2-1,l-1
+    for l2 >= 0 {
+        ans[l]=(*n1)[l1]+ (*n2)[l2] - '0' + supple
+        if ans[l] > '9' {
+            ans[l]-=10
+            supple = 1
+        } else {
+            supple = 0
+        }
+        l1--
+        l2--
+        l--
+    }
+    for l1 >= 0 {
+        ans[l]=(*n1)[l1] + supple
+        if ans[l] > '9' {
+            ans[l]-=10
+            supple = 1
+        } else {
+            supple = 0
+        }
+        l1--
+        l--
+    }
+    if supple == 0 {
+        return string(ans[1:])
+    }
+    ans[0] = '0'+ supple
+    return string(ans)
+    
 } 
-func helper(num1 , num2 []byte,ans []byte, l , end int, add byte) int { 
-    a := num1[l]
-    b := num2[l]
-    sum := a+b+add
-    if a!=0{
-        sum-='0'
-    }
-    if b!=0{
-        sum-='0'
-    }
-    ans[l] = sum%10+'0'
-    if end == -1 || (end==0 && sum%10==0){
-        return l
-    }
-    return helper(num1, num2, ans, l-1,end-1, sum/10)
-}
